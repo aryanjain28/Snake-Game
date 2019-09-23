f1e911c079faa67a87cecd801ca484d8510483c3
Aryan Jain <aryanjn807@gmail.com>
2019-09-23 16:08:51 +0530

    Update snakeGame.py

--- snakeGame.py
+++ snakeGame.py
@@ -1,103 +1,130 @@
 from tkinter import *
 import random
+import time
 
-y = 0
+points = 0
 forward = -1
 backward = -1
 right = -1
 left = -1
+y=0
+x=0
 
 class GAME:
 
     def __init__(self):
 
-        global y
+        global points
+        points = -1
+
+        self.size = 30
 
         self.x1 = 145
         self.x2 = 160
-        self.y1 = 150+y
-        self.y2 = 180+y
+        self.y1 = 150
+        self.y2 = 180
 
-        self.canvas = Canvas(mainWindow, height=300, width=300, bg='blue')
+        self.canvas = Canvas(mainWindow, height=500, width=500, bg='blue')
         self.canvas.pack()
 
         self.food = self.canvas.create_rectangle(20, 20, 30, 30, fill='white')
         self.snake = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill='red')
         self.tempRectangle = self.canvas.create_rectangle(0,0,0,0,fill='red')
 
-    def foodPosition(self, event):
+        self.point = Label(mainWindow, text=str(points))
+        self.point.pack()
+
+        self.encounter()
+
+    def encounter(self):
+
+        global forward, backward, right, left, x, y
 
         self.canvas.delete(self.food)
 
-        x1 = random.randrange(20, 280)
-        x2 = x1 + 10
+        self.foodX1 = random.randrange(20, 480)
+        self.foodX2 = self.foodX1 + 25
 
-        y1 = random.randrange(20, 280)
-        y2 = y1 + 10
+        self.foodY1 = random.randrange(20, 480)
+        self.foodY2 = self.foodY1 + 25
 
-        self.food = self.canvas.create_rectangle(x1, y1, x2, y2, fill='white')
+        self.food = self.canvas.create_rectangle(self.foodX1, self.foodY1, self.foodX2, self.foodY2, fill='white')
+
+
+        global points
+        points+=1
+        self.point.destroy()
+        self.point = Label(mainWindow, text=str(points))
+        self.point.pack()
+
 
     def snakeForward(self, event):
 
-        global forward, backward, left, right
+        global forward, backward, right, left
+
         forward = 1
         backward = 0
 
         self.canvas.delete(self.snake)
         self.canvas.delete(self.tempRectangle)
 
-        if left == -1 and right == -1:
+        if (((self.x1 > self.foodX1 and self.x1 < self.foodX2) and (
+                self.y1 > self.foodY1 and self.y1 < self.foodY2)) or
+              ((self.x2 > self.foodX1 and self.x2 < self.foodX2) and (
+                      self.y2 > self.foodY1 and self.y2 < self.foodY2))):
+            self.size += 30
+            self.encounter()
 
+        if abs((self.x1 - self.x2))>15:
+
+            if right == 1 or right == -1:
+                self.x1 += 10
+                self.tempRectangle = self.canvas.create_rectangle(self.x1 + 5, self.y1-10, self.x2, self.y1, fill='red')
+
+            elif left == 1 or left == -1:
+                self.x2 -= 10
+                self.tempRectangle = self.canvas.create_rectangle(self.x1, self.y1-10, self.x2-15, self.y2-15, fill='red')
+
+            if abs((self.x1 - self.x2)) < 15:
+                self.y2 = self.y1 + self.size
+                self.x2 = self.x1 + 15
+
+        else:
             self.y1 -= 10
             self.y2 -= 10
+            self.x2 = self.x1+15
 
-            self.snake = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill='red')
-
-
-        # global forward, backward
-        #
-        # forward = 0
-        # backward = 1
-        #
-        # self.canvas.delete(self.snake)
-        # self.canvas.delete(self.tempRectangle)
-        #
-        # if abs(self.x2-self.x1) == 15:
-        #
-        #
-        # if abs((self.y1 - self.y2)) > 15:
-        #
-        #     if forward == 1 or forward == -1:
-        #         self.y2 -= 10
-        #         self.tempRectangle = self.canvas.create_rectangle(self.x1 - 10, self.y1, self.x1, self.y1 + 15,
-        #                                                           fill='red')
-        #
-        #     elif backward == 1 or backward == -1:
-        #         self.y1 += 10
-        #         self.tempRectangle = self.canvas.create_rectangle(self.x2-25, self.y2 - 15, self.x2-15, self.y2,
-        #                                                           fill='red')
-        #
-        #     if abs((self.y1 - self.y2)) < 15:
-        #         self.y2 = self.y1 + 15
-        #         self.x2 = self.x1 + 30
-        #
-        # else:
-        #     self.x1 -= 10
-        #     self.x2 -= 10
-        #     self.y2 = self.y1 + 15
-        #
-        # self.snake = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill='red')
+        self.snake = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill='red')
 
 
     def snakeBackward(self, event):
 
-        global forward, backward
+        global forward, backward, right, left
 
+        forward = 0
+        backward = 1
 
         self.canvas.delete(self.snake)
+        self.canvas.delete(self.tempRectangle)
 
-        self.y1 += 10
-        self.y2 += 10
+        if abs((self.x1 - self.x2))>15:
+
+            if right == 1 or right == -1:
+                self.tempRectangle = self.canvas.create_rectangle(self.x1 + 15, self.y2, self.x1 + 30, self.y2 +10,fill='red')
+                self.x1 += 10
+
+            elif left == 1 or left == -1:
+                self.tempRectangle = self.canvas.create_rectangle(self.x1, self.y2, self.x2-15, self.y2+10, fill='red')
+                self.x2 -= 10
+
+            if abs((self.x1 - self.x2)) < 15:
+                self.y2 = self.y1 + 30
+                self.x2 = self.x1 + 15
+
+        else:
+            self.y1 += 10
+            self.y2 += 10
+            self.x2 = self.x1-15
 
         self.snake = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill='red')
 
@@ -134,6 +161,10 @@ class GAME:
 
         self.snake = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill='red')
 
+        if (((self.x1 > self.foodX1 and self.x1 < self.foodX2) and (self.y2 > self.foodY1 and self.y2 < self.foodY2)) or
+        ((self.x1 > self.foodX1 and self.x1 < self.foodX2) and (self.y1 > self.foodY1 and self.y1 < self.foodY2))):
+            self.encounter()
+
     def snakeRight(self, event):
 
         global forward, backward, right, left
@@ -165,6 +196,11 @@ class GAME:
 
         self.snake = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill='red')
 
+        if (((self.x2 > self.foodX1 and self.x2 < self.foodX2) and (self.y2 > self.foodY1 and self.y2 < self.foodY2)) or
+                ((self.x2 > self.foodX1 and self.x2 < self.foodX2) and (
+                        self.y1 > self.foodY1 and self.y1 < self.foodY2))):
+            self.encounter()
+
 
 
 
@@ -174,8 +210,6 @@ mainWindow = Tk()
 
 
 o = GAME()
-
-mainWindow.bind('<Double-Button-1>', o.foodPosition)
 
 mainWindow.bind('<Up>', o.snakeForward)
 mainWindow.bind('<Down>', o.snakeBackward)
